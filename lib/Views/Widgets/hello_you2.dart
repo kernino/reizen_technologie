8e48f1d1e3fc515111f6d5eddaa6b2928cd4ec85
Stijn Meerten <r0668329@ucll.be>
2019-10-13 18:22:23 +0200

    Inloggevens valideren

--- lib/Views/Widgets/hello_you2.dart
+++ lib/Views/Widgets/hello_you2.dart
@@ -6,11 +6,13 @@ import 'package:reizen_technologie/ViewModel/HelloYouViewModel.dart';
 
 class HelloYou2 extends StatelessWidget {
 
+  final String text;
+  HelloYou2({Key key, @required this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-        body: Text("Logged in! " + HelloYouViewModel.name)
+        body: Text("Api-token: " + text)
     );
   }
 }
