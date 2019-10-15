0f83bf166f9ede7e4271a2eaaf397a1f4830828f
kernino <ninokerkhofs@gmail.com>
2019-10-15 14:30:37 +0200

    Revert "Merge branches 'Sandbox' and 'master' of https://github.com/kernino/reizen_technologie"
    This reverts commit a334504ce94670db80ebbf3d64c0617d322da619, reversing
    changes made to 305abd1d7868ca8605ac9565eddb1fef05b692b6.

--- lib/Views/Widgets/hello_you2.dart
+++ lib/Views/Widgets/hello_you2.dart
@@ -1,18 +1,57 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:reizen_technologie/ViewModel/HelloYouViewModel.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
-
+import 'appbar.dart';
+import 'package:reizen_technologie/ViewModel/GetAlgemeneInfo.dart';
 
 class HelloYou2 extends StatelessWidget {
-
-  final String text;
-  HelloYou2({Key key, @required this.text}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        body: Text("Api-token: " + text)
+    final HttpLink httpLink =
+    HttpLink(uri: "http://171.25.229.102:8222/graphql?query=");
+    final AuthLink authLink = AuthLink(
+      getToken: () async => 'Bearer UkDSHeJHscD3wU5zmnSjXWQKLWZkWAz4vzs4TSuKAQrRXILDSL7iB9qy5Qhy',
+    );
+    final Link link = authLink.concat(httpLink);
+    final ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient>(
+      GraphQLClient(
+        link: link,
+        cache: OptimisticCache(
+          dataIdFromObject: typenameDataIdFromObject,
+        ),
+      ),
+    );
+    return GraphQLProvider(
+      child: AlgemeneInfo(),
+      client: client,
+    );
+  }
+}
+
+class AlgemeneInfo extends StatefulWidget {
+  @override
+  _AlgemeneInfo createState() => _AlgemeneInfo();
+}
+
+class _AlgemeneInfo extends State<AlgemeneInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppbarTest.getAppbar(),
+      body: Query(
+          options: QueryOptions(document: getAlgemeneData()),
+          builder: (QueryResult result, {
+            BoolCallback refetch,
+            FetchMore fetchMore,
+          }){
+            return ListView.builder(itemBuilder: (BuildContext context, int index){
+              index = 0;
+              return Text(result.data['info'][index]['info_value']);
+            },
+              itemCount: result.data['info'].length,
+            );
+          }),
     );
   }
 }
