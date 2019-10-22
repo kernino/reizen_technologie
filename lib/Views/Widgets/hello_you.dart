506eea6cb15fa6924c2e731ac2f8c2697f865c7e
kernino <ninokerkhofs@gmail.com>
2019-10-22 17:16:54 +0200

    Revert "Inloggevens valideren"
    This reverts commit 8e48f1d1e3fc515111f6d5eddaa6b2928cd4ec85.

--- lib/Views/Widgets/hello_you.dart
+++ lib/Views/Widgets/hello_you.dart
@@ -2,37 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:reizen_technologie/ViewModel/HelloYouViewModel.dart';
-import 'dart:async';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'package:reizen_technologie/Views/Widgets/hello_you2.dart';
 
-showAlertDialog(BuildContext context, String text) {
-  // set up the button
-  Widget okButton = FlatButton(
-    child: Text("OK"),
-    onPressed: () {
-      Navigator.of(context, rootNavigator: true).pop('dialog');
-    },
-  );
-
-  // set up the AlertDialog
-  AlertDialog alert = AlertDialog(
-    title: Text("Foutmelding"),
-    content: Text(text),
-    actions: [
-      okButton,
-    ],
-  );
-
-  // show the dialog
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return alert;
-    },
-  );
-}
 
 class HelloYou extends StatefulWidget {
   @override
@@ -40,47 +10,10 @@ class HelloYou extends StatefulWidget {
 }
 
 class _HelloYouState extends State<HelloYou> {
-
-  String status, message, api_token;
-  bool isData = false;
-
-  FetchJSON(String username, String password) async {
-    String url = 'http://171.25.229.102:8222/api/login';
-    Map<String, String> headers = {"Content-type": "application/json"};
-    String json = '{"username":"' + username + '","password":"' + password + '"}';
-    var Response = await http.post(url, headers: headers, body: json);
-
-    if (Response.statusCode == 200) {
-      String responseBody = Response.body;
-      var responseJSON = jsonDecode(responseBody);
-      status = responseJSON['status'];
-      message = responseJSON['message'];
-      api_token = responseJSON['api_token'];
-      isData = true;
-      setState(() {
-        print('UI Updated');
-      });
-    } else {
-      print('Something went wrong. \nResponse Code : ${Response.statusCode}');
-    }
-    if (status != null && api_token != null && message == null){
-      //Navigator.of(context).pushReplacementNamed('/screen2');
-      Navigator.pushReplacement(
-          context,
-          new MaterialPageRoute(
-              builder: (BuildContext context) =>
-              new HelloYou2(text: '$api_token')));
-    }
-    else if (status != null && api_token == null && message != null){
-      showAlertDialog(context, "Onjuiste inloggegevens gebruikt.");
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     HelloYouViewModel viewModel = new HelloYouViewModel();
-    final usernameController = TextEditingController();
-    final passwordController = TextEditingController();
+    final nameController = TextEditingController();
 
     return Container(
         padding: EdgeInsets.all(15.0),
@@ -94,31 +27,10 @@ class _HelloYouState extends State<HelloYou> {
                 shape: BoxShape.rectangle,
                 borderRadius: BorderRadius.circular(2),
               ),
-              child: new Column(
-                children: <Widget>[
-                  new TextFormField(
-                    style: TextStyle(color: Colors.black),
-                    keyboardType: TextInputType.text,
-                    controller: usernameController,
-                    ),
-                  new TextFormField(
-                    style: TextStyle(color: Colors.black),
-                    keyboardType: TextInputType.text,
-                    controller: passwordController,
-                    ),
-                  new Text(
-                    'Status : $status',
-                    style: Theme.of(context).textTheme.title,
-                  ),
-                  new Text(
-                    'api_token : $api_token',
-                    style: Theme.of(context).textTheme.title,
-                  ),
-                  new Text(
-                    'Message : $message',
-                    style: Theme.of(context).textTheme.title,
-                  ),
-                  ],
+              child: TextFormField(
+                style: TextStyle(color: Colors.black),
+                keyboardType: TextInputType.text,
+                controller: nameController,
               ),
             ),
             Container(
@@ -126,9 +38,10 @@ class _HelloYouState extends State<HelloYou> {
               child: RaisedButton(
                 color: Colors.blueAccent,
                 onPressed: () {
-                  FetchJSON(usernameController.text, passwordController.text);
+                  viewModel.setName(nameController.text.trim());
+                  Navigator.of(context).pushReplacementNamed('/screen2');
                 },
-                child: const Text("Login"),
+                child: Text("Login"),
               ),
             )
           ],
