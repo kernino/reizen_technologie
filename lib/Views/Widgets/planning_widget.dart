b52208db3769e251ccd89f0f9d398fe8ef3218d4
Pieter Coenen <r0668811@ucll.be>
2019-11-01 17:01:41 +0100

    planning_widget scrollable

--- lib/Views/Widgets/planning_widget.dart
+++ lib/Views/Widgets/planning_widget.dart
@@ -33,95 +33,101 @@ class _PlanningPageState extends State<PlanningPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Appbar.getAppbar(widget.title),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Title(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text(
-                    "City 1",
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      body: Scrollbar(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Title(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text(
+                        "City 1",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              color: Color.fromRGBO(0, 0, 0, 1.0),
-              title: "City 1",
-            ),
-            Card(
-              child: const ListTile(
-                title: Text('Day 1'),
-                subtitle: Text('17/05 - 18/05'),
-              ),
-            ),
-            Card(
-              child: const ListTile(
-                title: Text('Day 2'),
-                subtitle: Text('18/05 - 19/05'),
-              ),
-            ),
-            Card(
-              child: const ListTile(
-                title: Text('Day 3'),
-                subtitle: Text('19/05 - 20/05'),
-              ),
-            ),
-            Title(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text(
-                    "City 2",
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  color: Color.fromRGBO(0, 0, 0, 1.0),
+                  title: "City 1",
+                ),
+                Card(
+                  child: const ListTile(
+                    title: Text('Day 1'),
+                    subtitle: Text('17/05 - 18/05'),
                   ),
-                ],
-              ),
-              color: Color.fromRGBO(0, 0, 0, 1.0),
-              title: "City 2",
-            ),
-            Card(
-              child: const ListTile(
-                title: Text('Day 4'),
-                subtitle: Text('21/05 - 22/05'),
-              ),
-            ),
-            Card(
-              child: const ListTile(
-                title: Text('Day 5'),
-                subtitle: Text('22/05 - 23/05'),
-              ),
-            ),
-            Card(
-              child: const ListTile(
-                title: Text('Day 3'),
-                subtitle: Text('24/05 - 25/05'),
-              ),
-            ),
-            Title(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text(
-                    "City 3",
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                Card(
+                  child: const ListTile(
+                    title: Text('Day 2'),
+                    subtitle: Text('18/05 - 19/05'),
                   ),
-                ],
-              ),
-              color: Color.fromRGBO(0, 0, 0, 1.0),
-              title: "City 3",
+                ),
+                Card(
+                  child: const ListTile(
+                    title: Text('Day 3'),
+                    subtitle: Text('19/05 - 20/05'),
+                  ),
+                ),
+                Title(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text(
+                        "City 2",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  color: Color.fromRGBO(0, 0, 0, 1.0),
+                  title: "City 2",
+                ),
+                Card(
+                  child: const ListTile(
+                    title: Text('Day 4'),
+                    subtitle: Text('21/05 - 22/05'),
+                  ),
+                ),
+                Card(
+                  child: const ListTile(
+                    title: Text('Day 5'),
+                    subtitle: Text('22/05 - 23/05'),
+                  ),
+                ),
+                Card(
+                  child: const ListTile(
+                    title: Text('Day 3'),
+                    subtitle: Text('24/05 - 25/05'),
+                  ),
+                ),
+                Title(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text(
+                        "City 3",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  color: Color.fromRGBO(0, 0, 0, 1.0),
+                  title: "City 3",
+                ),
+                Card(
+                  child: const ListTile(
+                    title: Text('Day 7'),
+                    subtitle: Text('26/05 - 27/05'),
+                  ),
+                ),
+              ],
             ),
-            Card(
-              child: const ListTile(
-                title: Text('Day 7'),
-                subtitle: Text('26/05 - 27/05'),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
   }
 }
-
