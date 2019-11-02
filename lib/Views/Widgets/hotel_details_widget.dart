d793adac52ff7b817830f2491efcd470b36852ab
Sibert Schurmans <sibert.schurmans@hotmail.com>
2019-11-02 18:41:14 +0100

    Bug fixing

--- lib/Views/Widgets/hotel_details_widget.dart
+++ lib/Views/Widgets/hotel_details_widget.dart
@@ -15,28 +15,40 @@ class HotelDetailsPage extends StatefulWidget {
 class _HotelDetailsPageState extends State<HotelDetailsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: new FutureBuilder(
+    return FutureBuilder(
           future: GetHotelData(widget.hotel),
           builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
-            var content = snapshot.data;
-            return Scaffold(
-              appBar: Appbar.getAppbar(
-                content[0]['name'],
-                IconButton(
-                    icon: Icon(Icons.arrow_back),
-                    onPressed: () => Navigator.pop(
-                          context,
-                          MaterialPageRoute(builder: (context) => HotelsPage()),
-                        )),
-              ),
-              body: new Column(
-                children: <Widget>[
-                  Text(content[0]['description']),
-                ],
-              ),
-            );
-          }),
-    );
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return new Container();
+              case ConnectionState.active:
+                return new Container();
+              case ConnectionState.waiting:
+                return new Container();
+              case ConnectionState.done:
+                var content = snapshot.data;
+                return new Scaffold(
+                  appBar: Appbar.getAppbar(
+                    content[0]['name'],
+                    IconButton(
+                        icon: Icon(Icons.arrow_back),
+                        onPressed: () =>
+                            Navigator.pop(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HotelsPage()),
+                            )),
+                  ),
+                  body: new Column(
+                    children: <Widget>[
+                      Text(content[0]['description']),
+                    ],
+                  ),
+                );
+            }
+            return new Container();
+          });
+
+
   }
 }
