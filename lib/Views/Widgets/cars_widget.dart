763037be9f8729006db15b27645172e070e23519
kernino <ninokerkhofs@gmail.com>
2019-11-18 09:32:36 +0100

    Cars fix for many passengers

--- lib/Views/Widgets/cars_widget.dart
+++ lib/Views/Widgets/cars_widget.dart
@@ -32,11 +32,8 @@ class _CarsPageState extends State<CarsPage> {
         }
         content = snapshot.data;
         return new Scrollbar(
-          child: GridView.count(
-              crossAxisCount: 1,
+          child: ListView(
               padding: EdgeInsets.all(10.0),
-              mainAxisSpacing: 10.0,
-              crossAxisSpacing: 10.0,
               children:
               List.generate(content.length, (index){
                      return
@@ -47,6 +44,8 @@ class _CarsPageState extends State<CarsPage> {
                       ),
                       child: makeWidget(content[index],index),
                       padding: EdgeInsets.all(5.0),
+                         margin: EdgeInsets.all(5.0),
+
                     );
           }).toList(),
           )
