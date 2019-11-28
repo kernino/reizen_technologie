5ee620cbce6fd69b4cf617655b25666be19d7077
Daan Van Hirtum <daanvanhirtum@icloud.com>
2019-11-28 17:42:34 +0100

    twee functies ophalen uit lokale database VandaagView

--- lib/ViewModel/VandaagViewModel.dart
+++ lib/ViewModel/VandaagViewModel.dart
@@ -1,5 +1,13 @@
 import 'package:reizen_technologie/Model/globals.dart' as globals;
 
+Future<List> GetAllData() async {
+  List<Map> allData = new List();
+  List<Map> Users = await GetUser();
+  allData.add({'user':Users[0],'day_planning':GetPlanning()});
+
+  return allData;
+}
+
 Future<List> GetUser() async {
   List<Map> userData = await globals.database.query('users');
   if (userData != null) {
@@ -9,4 +17,16 @@ Future<List> GetUser() async {
     print("data user ophalen NIET gelukt: ");
   }
   return userData;
+}
+
+
+Future<List> GetPlanning() async {
+  List<Map> planningData = await globals.database.query('day_planning');
+  if (planningData != null) {
+    print("data day_planning ophalen gelukt: " + planningData.toString());
+  }
+  else {
+    print("data day_planning ophalen NIET gelukt: ");
+  }
+  return planningData;
 }
